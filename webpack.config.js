b7647769649ea56fdc99e820852be9db00bec19e
AlexGrin1 <a_grynevich@mail.ru>
2021-07-20 02:04:01 +0300

    add slider

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,39 @@
-const path = require("path");
-const HTMLwebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+const webpack = require('webpack');
+const HTMLwebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-  mode: "development",
-  entry: "./src/js/app.js",
+  mode: 'development',
+  entry: './src/js/app.js',
   output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
   },
   plugins: [
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      jquery: 'jquery',
+      jQuery: 'jquery',
+      'window.$': 'jquery',
+      'window.jquery': 'jquery',
+      'window.jQuery': 'jquery',
+    }),
     new HTMLwebpackPlugin({
-      template: "./src/index.html",
+      template: './src/index.html',
     }),
     new CleanWebpackPlugin(),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
-      chunkFilename: "[id].css",
+      filename: '[name].css',
+      chunkFilename: '[id].css',
     }),
     new CopyWebpackPlugin({
       patterns: [
         {
-          from: "./src/assets",
-          to: "./assets",
+          from: './src/assets',
+          to: './assets',
         },
       ],
     }),
@@ -33,19 +42,19 @@ module.exports = {
     rules: [
       {
         test: /\.(s*)css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
       {
         test: /\.(png|jpg|svg|gif)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
       {
         test: /\.(ttf|woff|woff2|eat)$/,
-        use: ["file-loader"],
+        use: ['file-loader'],
       },
     ],
   },
